ec5116ec1ea8a60c3d6dccf32779c431232f5287
AaronRmj <aaronrmjk@gmail.com>
2026-03-17 17:25:25 +0300

    Appel du resulat du preproc dans le fichier intent

--- backend/nlu_feature/intent_classifier.py
+++ backend/nlu_feature/intent_classifier.py
@@ -1,4 +1,12 @@
 import math
 
-from preprocessing import nettoyer_phrase
+from preprocessing import nettoyer_dataset, charger_dataset, nettoyer_phrase
+
 K_value = 3 #Pour l'algo KNN
+
+#on appelle le resulat du chargement des données
+contenu = charger_dataset()
+data, vocabulaire = nettoyer_dataset(contenu)
+
+# data va contenir les intentions, et vocabluaire les mots uniques
+
