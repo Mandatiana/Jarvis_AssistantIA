448b6a913e6e9ddf0d83633d913d363d39c2a062
AaronRmj <aaronrmjk@gmail.com>
2026-03-21 18:16:47 +0300

    Optimiser code Lancement musique

--- backend/nlu_feature/execution.py
+++ backend/nlu_feature/execution.py
@@ -1,39 +1,32 @@
 import os 
 import webbrowser
 import urllib.parse
-
+from youtubesearchpython import VideosSearch
 
 from intent_classifier import interpreter_commande
-
-# Recuperer input user et commande demandé venant de la classification
-#Pour eviter qu il execute une deuxieme fois
-if __name__ == "__main__":
-    resulat_knn, phrase_user = interpreter_commande()
-
-
-def executer_commande(prediction):
+def executer_commande(prediction, phrase_user):
     if prediction == "ouvrir_chrome":
         webbrowser.open("https://www.google.com")
         print("J'ai ouvert chrome pour vous")
-        
-executer_commande(resulat_knn)
-
     
-def lancer_musique(prediction, phrase_brute):
-    if prediction == 'play_song':
+    elif prediction == 'play_song':
         verbes_musique = ["jouer","lancer", "chanson", "morceau", "playlist", "musique", "lancer","mettre","mets","diffuser","ecouter"]
-        recherche = [m for m in phrase_brute if m not in verbes_musique]
+        recherche = [m for m in phrase_user if m not in verbes_musique]
 
         #Espace dans le titre
         requete = " ".join(recherche)
         if requete:
             print(f"Recherche de {requete} sur Youtube")
 
-            #transforme les espaces en %20 ou +
+            #transformer les espaces en %20 ou +
             query_encode = urllib.parse.quote(requete)
             webbrowser.open(f"https://www.google.com/search?q={query_encode}+youtube&btnI")
         else:
             print("Quel musique veux tu ecouter? ")
 
-
-lancer_musique(resulat_knn,phrase_user)
+#Ne s'execute que lorsque je lance ce fichier uniquement meme si ce fichier est importé ailleurs
+if __name__ == "__main__":
+    resulat  = interpreter_commande()
+    resulat_knn, phrase_user = resulat
+    if resulat is not None:
+        executer_commande(resulat_knn, phrase_user)
