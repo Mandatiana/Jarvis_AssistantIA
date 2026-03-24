a211c8b71ebd66b185320a2fdce37a9e9c2450ec
AaronRmj <aaronrmjk@gmail.com>
2026-03-24 16:29:39 +0300

    new askjarvis

--- backend/Jarvis/services/AskJarvis.py
+++ backend/Jarvis/services/AskJarvis.py
@@ -4,25 +4,34 @@ import os
 
 load_dotenv()  
 
-url = "https://models.github.ai/inference/chat/completions"
-ApiKey =os.getenv('API_KEY')
 
-headers = {
-    "Accept": "application/vnd.github+json",
-    "Authorization": f"Bearer {ApiKey}",
-    "X-GitHub-Api-Version": "2022-11-28",
-    "Content-Type": "application/json"
-}
+#Modification pour l'importer dans le KNN (response non programmé)
 
-data = {
-    "model": "openai/gpt-4.1",
-    "messages": [
-        {"role": "user", "content": "Comment sera la meteo ce soir"}
-    ]
-}
+def ask_jarvis(question):
+    url = "https://models.github.ai/inference/chat/completions"
+    ApiKey =os.getenv('API_KEY')
 
-response = requests.post(url, headers=headers, json=data)
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {ApiKey}",
+        "X-GitHub-Api-Version": "2022-11-28",
+        "Content-Type": "application/json"
+    }
 
-print(response.status_code)
-#print(response.json())
-print(response.json()["choices"][0]["message"]["content"])
+    data = {
+        "model": "openai/gpt-4o-mini",
+        "messages": [
+            {"role": "system", "content": "Tu es JARVIS. Tu dois répondre EXCLUSIVEMENT en français, de manière concise et polie. Ne réponds jamais dans une autre langue"},
+            {"role": "user", "content": question}
+        ]
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            reponse_texte = response.json()["choices"][0]["message"]["content"]
+            return reponse_texte
+        else:
+            return "Désolé monsieur, je recontre des difficultés de connexion"
+    except Exception as e:
+        return f"Erreur de communication: {e}"
